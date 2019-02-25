773e3731f41e14591f42c2f1817dc85dffb465b9
mitchell-lawrence <47456067+mitchell-lawrence@users.noreply.github.com>
2019-02-24 20:35:13 -0700

    Add files via upload
    See notes in code. Should work as expected for our presentation. More features may be added with more time

---
+++ Ship.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
+import java.util.Scanner;
+
+
+public class Ship {
+	/*Some instance stuff. A list and whatever. Note that the list */
+	Scanner keyboard = new Scanner(System.in);
+	ArrayList<String> aShipCharList= new ArrayList<String>();
+	public String shipChar;	
+	
+	/*the ship constructor*/
+	Ship(String shipChar){
+		
+		/*
+		*Some super elegant list creation. Hope it doesnt blow your minds.
+		*/
+		
+
+		
+		if (shipChar.equals("D") || shipChar.equals("d")){
+			
+			aShipCharList.add("D");
+			aShipCharList.add("D");
+		}
+		
+		else if (shipChar.equals("s") || shipChar.equals("S")){
+				
+			aShipCharList.add("S");
+			aShipCharList.add("S");
+			aShipCharList.add("S");
+			
+		}
+		else if (shipChar.equals("c") || shipChar.equals("C")){
+		
+			aShipCharList.add("C");
+			aShipCharList.add("C");
+			aShipCharList.add("C");
+			
+		}
+		
+		else if (shipChar.equals("B") || shipChar.equals("b")){
+		
+			aShipCharList.add("B");
+			aShipCharList.add("B");
+			aShipCharList.add("B");
+			aShipCharList.add("B");
+			
+		}
+		
+		else if (shipChar.equals("A") || shipChar.equals("a")){
+		
+			aShipCharList.add("A");
+			aShipCharList.add("A");
+			aShipCharList.add("A");
+			aShipCharList.add("A");
+			aShipCharList.add("A");
+		}
+		
+		else{
+			/*A sarcastic error message I used in debugging*/
+			System.out.println("nice try");
+		}
+		/*This could also be removed. I was just using it to make sure the ships were the
+		* right size and character
+		*/
+		System.out.println(aShipCharList);
+	}
+	 
+	 /*
+	 *Another really boring main method. Should make sense though, I hope.
+	 */
+	public static void main(String[] args) {
+		
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("What the Ship?! ");
+		
+		
+
+		String aShipChar = keyboard.nextLine ();
+		
+		Ship playerShip = new Ship(aShipChar);
+	
+		
+		
+	}
+}
+	
+	
+	
