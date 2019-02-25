b02b6b517c69d15d76af0dc19036d8809dd29b67
merissagreer <47456065+merissagreer@users.noreply.github.com>
2019-02-24 21:53:58 -0700

    Add skeleton classes
    Added HitOrMiss and PlayerActions skeleton classes

---
+++ PlayerActions.java
@@ -0,0 +1,21 @@
+
+public class PlayerActions {
+	
+	int numOfTurns = 3;
+	
+	public void moveShip() {
+		
+	}
+	
+	public void attack() {
+		HitOrMiss.trueHit(0,0);
+	}
+	
+	public static void main(String[] args) {
+
+		System.out.println("You have 3 turns");
+		
+
+	}
+
+}
