ef71a61d6e32c1fb8802213c00eda378d6509e94
karienal <ajhemplo@gmail.com>
2019-03-25 21:13:49 -0600

    Deprecated Weapons class / moved code into Player

--- Player.java
+++ Player.java
@@ -26,6 +26,49 @@ public abstract class Player {
 
 	public abstract void attack();
 
+	public void attack(String weapon) {
+		if (weapon == "default") {
+			attack();
+		}
+		if (weapon == "beacon") {
+			placeBeacon();
+		}
+		/*if (weapon == "plus") { no code for the + attack yet
+			plusAttack();
+		}*/
+	}
+	//I suggest moving this to HitOrMiss, but I'm unsure
+	public void placeBeacon(BattleshipBoard enemyBoard, BattleshipBoard displayEnemyBoard, int row, int column) {
+		String selectedTile = enemyBoard.getBoard()[row][column];
+		if(row > 0 && row < 12) {
+			if(column > 0 && column < 12) {
+				int rowPlus2 = row + 2;
+				int columnPlus2 = column + 2;
+				while(row <= rowPlus2) {
+					while(column <= columnPlus2) {
+						if(selectedTile == " . ") {
+							enemyBoard.getBoard()[row][column] = " O ";
+							displayEnemyBoard.getBoard()[row][column] = " . ";
+						}
+						else if(selectedTile == " O "){
+							enemyBoard.getBoard()[row][column] = " O ";
+							displayEnemyBoard.getBoard()[row][column] = " O ";
+						}
+						else if(selectedTile == " R "){
+							enemyBoard.getBoard()[row][column] = " X ";
+							displayEnemyBoard.getBoard()[row][column] = " X ";
+						}
+						else {
+							enemyBoard.getBoard()[row][column] = " x ";
+							displayEnemyBoard.getBoard()[row][column] = " x ";
+						}
+					}
+				}
+			}
+		}
+
+	}
+
 	public abstract void repair();
 	
 	public ArrayList<String> getShipList() {
@@ -37,4 +80,6 @@ public abstract class Player {
 	public BattleshipBoard getYourBoard() {
 		return yourBoard;
 	}
+	
+	
 }
