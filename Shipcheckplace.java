ffa5cde8ef73ced8488e55b8e097517083ef43ff
SirSaberlot <47456158+SirSaberlot@users.noreply.github.com>
2019-03-26 12:24:07 -0600

    Add files via upload
    I add the logic for overlap, I am still confused on the rows and columns bit. That part might be flipped. Working on controller for gui still lost.

--- Shipcheckplace.java
+++ Shipcheckplace.java
@@ -1,7 +1,7 @@
 
 public class Shipcheckplace {
 	
-	public  boolean validShip(BattleshipBoard aBoard,int startCol, int startRow, String shipChar , String orientation) {
+	public  boolean validShipCol(BattleshipBoard aBoard,int startCol, int startRow, String shipChar , String orientation) {
 		boolean placeship = false;
 		String shipCheck = shipChar;
 		shipCheck.toUpperCase();
@@ -16,12 +16,24 @@ public class Shipcheckplace {
 				return  placeship = true;
 			}
 			else if(shipCheck.equals("B")&& startCol<=7) {
-				return  placeship = true;
+			 	return  placeship = true;
 			}
 			else if(shipCheck.equals("A")&& startCol<=6) {
 				return  placeship = true;
 			}
 		}
+		
+		return placeship;
+		
+	}
+	/*
+	 * this checks for bounds for the bounds on the y axis
+	 */
+	public  boolean validShipRow(BattleshipBoard aBoard,int startCol, int startRow, String shipChar , String orientation) {
+		boolean placeship = false;
+		String shipCheck = shipChar;
+		shipCheck.toUpperCase();
+		
 		if (orientation.equals("x")|| orientation.equals("X")) {
 			if(shipCheck.equals("D") && startRow<=9) {
 				return placeship==true;
@@ -41,5 +53,49 @@ public class Shipcheckplace {
 		}
 		return placeship;
 	}
+	public boolean overlapCheck(BattleshipBoard aBoard,int startCol, int startRow, String shipChar , String orientation) {
+		boolean overlapCheck = false;
+		int shipSpot;
+		
+		Ship playerShip = new Ship(shipChar);
+		
+		int shipLength = playerShip.aShipCharList.size()+1;
+		/*
+		 * this is a for loop that checks for overlap for the ships
+		 * the loop will break and return the overlapCheck if it has found a overlap.
+		 * this only checks if the y orientation is over laping with another ship.
+		 */
+		if (orientation.equals("y") == true){
+			for (shipSpot = startRow; shipSpot <= startRow+shipLength; shipSpot++){
+					if (aBoard.getBoard()[shipSpot][startCol]!=" . "){
+						overlapCheck=false;}
+					/*
+					 * this breaks the loop
+					 */
+					else{
+						overlapCheck=true;
+						shipSpot = startRow+shipLength + 1;
+						
+						}
+					}
+				}
+		if (orientation.equals("x") == true){
+			for (shipSpot = startCol; shipSpot <= startCol+shipLength; shipSpot++){
+					if (aBoard.getBoard()[startRow][shipSpot]!=" . "){
+						overlapCheck=false;}
+					/*
+					 * this breaks the loop
+					 */
+					else{
+						overlapCheck=true;
+						shipSpot = startCol+shipLength + 1;
+						
+						}
+					}
+				}
+		return overlapCheck;
+	}
+	
+	
 	
 }
